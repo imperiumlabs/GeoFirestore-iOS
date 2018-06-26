2c2e192ed3737864ab999db1fd820fc9d9e1f945
Dhruv Shah <dhruv.shah@gmail.com>
2018-06-26 12:19:23 -0700

    Initial Commit

---
+++ Geofirestore/Geofirestore.h
@@ -0,0 +1,19 @@
+//
+//  Geofirestore.h
+//  Geofirestore
+//
+//  Created by Dhruv Shah on 6/26/18.
+//  Copyright © 2018 hello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Geofirestore.
+FOUNDATION_EXPORT double GeofirestoreVersionNumber;
+
+//! Project version string for Geofirestore.
+FOUNDATION_EXPORT const unsigned char GeofirestoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geofirestore/PublicHeader.h>
+
+
