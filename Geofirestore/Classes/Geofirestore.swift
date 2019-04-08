e495214eea9cf852df28066ff02afa7fc4ef687a
Alexey Solovyov <solovav@gmail.com>
2019-04-08 12:20:13 +0600

    Fix #15 GFSQuery .reset() Dead loop

--- Geofirestore/Classes/Geofirestore.swift
+++ Geofirestore/Classes/Geofirestore.swift
@@ -476,21 +476,16 @@ public class GFSQuery {
     }
     
     internal func reset() {
-        if !queries.isEmpty {
-            for query: GFGeoHashQuery? in queries {
-                var handle: GFSGeoHashQueryListener?
-                if let aQuery = query {
-                    handle = self.handles[aQuery]
-                    if handle == nil {
-                        NSException.raise(.internalInconsistencyException, format: "Wanted to remove a geohash query that was not registered!", arguments: getVaList(["nil"]))
-                    }
-                    handle?.childAddedListener?.remove()
-                    handle?.childChangedListener?.remove()
-                    handle?.childRemovedListener?.remove()
-                }
-                
+        for query in queries {
+            guard let handle = self.handles[query] else {
+                NSException.raise(.internalInconsistencyException, format: "Wanted to remove a geohash query that was not registered!", arguments: getVaList(["nil"]))
+                return
             }
+            handle.childAddedListener?.remove()
+            handle.childChangedListener?.remove()
+            handle.childRemovedListener?.remove()
         }
+
         locationInfos.removeAll()
         queries.removeAll()
         handles.removeAll()
